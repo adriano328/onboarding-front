bb94b6e5e0c4efb07860bfbbaabf9e448417f8a1
adriano328 <lucasadrianodias@gmail.com>
2022-08-01 15:48:10 -0400

    implementação classe produto

--- src/app/produto/produto-list/ProdutoListComponent.ts
+++ src/app/produto/produto-list/ProdutoListComponent.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { SituacaoEnum, SituacaoLabel } from 'src/app/shared/interfaces/enums/situacaoEnum';
+import { IProduto } from 'src/app/shared/interfaces/IProduto';
 
 
 @Component({
@@ -7,10 +11,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./produto-list.component.scss']
 })
 export class ProdutoListComponent implements OnInit {
-  
-  ngOnInit(): void {
-    
+
+  nome: string = "";
+
+  form!:FormGroup;
+
+  ProdutoSave: IProduto = {} as IProduto; 
+
+  situacaoSelect!: number;
+
+  listCategoria: any = [];
+
+  public SituacaoLabel = SituacaoLabel;
+
+  public situacoes = Object.values(SituacaoEnum);
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: ActivatedRoute
+  ){
+
   }
 
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      nome:[''],
+      situacao: ['']
+    })
+
+  }
+
+  findByNome(){
+
+  }
+
+  resetForm(){
+
+  }
   
 }
